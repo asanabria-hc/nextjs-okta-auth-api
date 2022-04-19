4f0c1140dbd8b965abb4d990ce90c30375daec31
Andres Sanabria <asanabria@healthcare.com>
2022-04-19 14:13:32 -0600

    initial commit

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,27 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
+import type { OktaAuthOptions } from "@okta/okta-auth-js";
+import { OktaAuth } from "@okta/okta-auth-js";
+
+const oktaAuthConfig: OktaAuthOptions = {
+  issuer: "https://<okta-domain>/oauth2/default",
+  clientId: "<okta-client-id>",
+  redirectUri: "<redirect-uri>",
+  responseType: "code",
+  scopes: ["openid", "email", "offline_access"],
+  responseMode: "fragment",
+};
+
+const oktaAuth = new OktaAuth(oktaAuthConfig);
 
 type Data = {
-  name: string
-}
+  name: string;
+};
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  console.log(oktaAuth.options);
+  res.status(200).json({ name: "John Doe" });
 }
